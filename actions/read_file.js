a43aa723cb03bfdda15b997a06b68dc4bcb9d8d5
WFH Brian <brian@plexm.com>
2024-07-23 09:27:45 -0400

    switch -> conditional

--- actions/read_file.js
+++ actions/read_file.js
@@ -34,21 +34,20 @@
 async function read_file(env, params) {
   const file_path = params.file_path;
   const file_type = file_path.split('.').pop();
-  console.log({file_type});
+  // console.log({file_type});
   try {
     const resp = {
       path: file_path,
       content: '',
     };
-    switch(file_type) {
-      case 'pdf':
-        const file_name = file_path.split('/').pop();
-        resp.name = file_name;
-        resp.mime_type = 'application/pdf';
-        resp.content = env.fs.readFileSync(file_path, 'base64');
-        break;
-      default:
-        resp.content = (await env.fs.readFile(file_path)).toString();
+    if(file_type === 'pdf') {
+      const file_name = file_path.split('/').pop();
+      resp.name = file_name;
+      resp.mime_type = 'application/pdf';
+      resp.content = env.fs.readFileSync(file_path, 'base64');
+    }else{
+      // resp.content = (await env.fs.readFile(file_path)).toString();
+      resp.content = env.fs.readFileSync(file_path, 'utf-8');
     }
     // console.log(content); // ChatGPT can read this buffer, cool!
     return resp;
